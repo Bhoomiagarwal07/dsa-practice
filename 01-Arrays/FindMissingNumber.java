e7d58e902ec6b2e125f7af0e7f5d6a0139ec75b1
Bhoomi Agarwal <bhoomiagarwal071204@gmail.com>
2026-09-01 19:28:39 +0530

    solve find missing number

---
+++ 01-Arrays/FindMissingNumber.java
@@ -0,0 +1,23 @@
+public class FindMissingNumber {
+    public static int findXor(int arr[] , int n ){
+        
+        int xor1 = 0;
+        int xor2 =0;
+
+        for(int i=0;i<n-1;i++){
+            xor1 = xor1 ^ arr[i];
+            xor2 = xor2 ^ (i+1);
+
+        }
+
+        xor2 = xor2 ^ n;
+        return xor1 ^ xor2;
+
+    }
+    public static void main(String[] args) {
+        int num[] ={1,2,4,5};
+        int n =5;
+        System.out.println(findXor(num, n));
+        
+    }
+}
